78cf8c440b344b76c6d288000734f50c22202252
sumibane <sum123.ban@gmail.com>
2020-10-11 10:30:33 +0530

    Signup Fixed

--- proman-api/target/generated-sources/io/swagger/api/ApiException.java
+++ proman-api/target/generated-sources/io/swagger/api/ApiException.java
@@ -1,6 +1,6 @@
 package io.swagger.api;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-10-09T12:59:37.277+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-10-10T19:49:43.962+05:30")
 
 public class ApiException extends Exception{
     private int code;
--- proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
+++ proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
@@ -2,7 +2,7 @@ package io.swagger.api;
 
 import javax.xml.bind.annotation.XmlTransient;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-10-09T12:59:37.277+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-10-10T19:49:43.962+05:30")
 
 @javax.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
